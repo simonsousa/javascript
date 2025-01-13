8fef9efbbff5f438358fba23d4924a3961542c04
devSimon24 <simonsousa335@gmail.com>
2025-01-12 22:21:33 -0300

    adição de novos exercicios em js

---
+++ Aula13/ambiente.js
@@ -0,0 +1,11 @@
+var contador = 1
+while( contador<=6 ) {
+    console.log(`Passo ${contador}`)
+    contador++
+}
+
+var contador2 = 1
+do {
+    console.log('Tudo Bem?')
+    contador2++
+}while( contador<=6 )
---
+++ Aula14/ambiente.js
@@ -0,0 +1,12 @@
+// var c = 1
+// do {
+//     console.log(c)
+//     c++
+// }while (c <= 5)
+//-------------------------------------------------------
+
+console.log('Vai começar.......')
+for( var c = 1;c <= 4; c++ ) {
+    console.log(c)
+}
+console.log('FIM!')
